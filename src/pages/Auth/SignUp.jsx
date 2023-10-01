b72ae06a7c6b61762f5b92933764bc49c9febc46
JuanPabloRP <jpabloroldanp@gmail.com>
2023-09-30 21:37:47 -0500

    fix: minor style bug was fixed en signup page

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -134,7 +134,7 @@ const SignUp = () => {
 							required
 							onChange={(e) => onChangePassword(e, 'confirmarPassword')}
 							value={passwords.confirmarPassword}
-							className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2  border-green-800 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer  ${
+							className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2  border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer  ${
 								passwords.confirmarPassword.length > 0 &&
 								passwords.password.length > 0
 									? passwords.confirmarPassword === passwords.password
